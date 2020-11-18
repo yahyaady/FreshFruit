934d4b7952da9449691413c1f4698e9a001de6d4
yahyaardiyanto <yahyaardi33@gmail.com>
2020-11-18 11:53:30 +0700

    Fresh Fruit

---
+++ FreshFruit/FreshFruit/Controller/BucketController.cs
@@ -0,0 +1,54 @@
+﻿using FreshFruit.Model;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Text;
+using FreshFruit.Model;
+using System.Linq;
+
+namespace FreshFruit.Controller
+{
+    class BucketController
+    {
+        private Bucket bucket;
+        private BucketEventListener eventListener;
+
+        public BucketController(Bucket bucket, BucketEventListener eventListener)
+        {
+            this.bucket = bucket;
+            this.eventListener = eventListener;
+        }
+        public void addFruit(Fruit fruit)
+        {
+            if (bucketIsOverload())
+            {
+                eventListener.onFailed("Ops, Maaf Keranjang Sudah Penuh");
+            }
+            else
+            {
+                this.bucket.insert(fruit);
+                eventListener.onSucceed("Ah Mantap, Buah Berhasil Ditambahkan");
+
+            }
+        }
+        public bool bucketIsOverload()
+        {
+            return bucket.countItems() >= bucket.getCapacity();
+        }
+        public void removeFruit(Fruit fruit)
+        {
+            for (int itemPosition = 0; itemPosition < bucket.countItems(); itemPosition++)
+            {
+                if (bucket.findAll().ElementAt(itemPosition).getName() == fruit.getName())
+                {
+                    bucket.remove(itemPosition);
+                    eventListener.onSucceed("Berhasil dihapus");
+                }
+            }
+        }
+        public List<Fruit> findAll()
+        {
+            return this.bucket.findAll();
+        }
+    }
+}
---
+++ FreshFruit/FreshFruit/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using FreshFruit.Controller;
+using FreshFruit.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FreshFruit
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, BucketEventListener
+    {
+        Seller yahya;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Bucket keranjangBuah = new Bucket(2);
+            BucketController bucketController = new BucketController(keranjangBuah, this);
+
+            yahya = new Seller("yahya", bucketController);
+            listBoxBucket.ItemsSource = keranjangBuah.findAll();
+        }
+
+        public void onFailed(string massage)
+        {
+            MessageBox.Show(massage);
+        }
+
+        public void onSucceed(string massage)
+        {
+            listBoxBucket.Items.Refresh();
+        }
+
+        private void OnButtonAddanggurClicked(object sender, RoutedEventArgs e)
+        {
+            Fruit anggur = new Fruit("Anggur");
+            yahya.addFruit(anggur);
+
+            listBoxBucket.Items.Refresh();
+        }
+
+        private void OnButtonAddAppleClicked(object sender, RoutedEventArgs e)
+        {
+            Fruit apple = new Fruit("Apple");
+            yahya.addFruit(apple);
+
+            listBoxBucket.Items.Refresh();
+        }
+
+        private void OnButtonAddBananaClicked(object sender, RoutedEventArgs e)
+        {
+            Fruit banana = new Fruit("Banana");
+            yahya.addFruit(banana);
+
+            listBoxBucket.Items.Refresh();
+        }
+
+        private void OnButtonAddOrangeClicked(object sender, RoutedEventArgs e)
+        {
+            Fruit orange = new Fruit("Orange");
+            yahya.addFruit(orange);
+
+            listBoxBucket.Items.Refresh();
+        }
+    }
+}
